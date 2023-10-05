96f6eb4c0b6c41cbba59d9f8457200857e453627
tanpreetsingh <79357289+tanpreetsingh@users.noreply.github.com>
2023-10-05 14:33:43 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,62 +1,22 @@
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
+/** @flow */
 
-const {app, BrowserWindow, shell} = require('electron'); // Module to create native browser window.
-const {join} = require('path');
-const os = require('os');
+// This test harness mounts each test app as a separate root to test multi-root applications.
 
-const argv = require('minimist')(process.argv.slice(2));
-const projectRoots = argv._;
+import * as React from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './apps/index';
 
-let mainWindow = null;
+function mountApp() {
+  const container = document.createElement('div');
 
-app.on('window-all-closed', function () {
-  app.quit();
-});
+  ((document.body: any): HTMLBodyElement).appendChild(container);
 
-app.on('ready', function () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    icon: join(__dirname, 'icons/icon128.png'),
-    frame: false,
-    //titleBarStyle: 'customButtonsOnHover',
-    webPreferences: {
-      contextIsolation: true, // protect against prototype pollution
-      enableRemoteModule: false, // turn off remote
-      sandbox: false, // allow preload script to access file system
-      preload: join(__dirname, 'preload.js'), // use a preload script to expose node globals
-    },
-  });
-
-  // set dock icon for macos
-  if (os.platform() === 'darwin') {
-    app.dock.setIcon(join(__dirname, 'icons/icon128.png'));
-  }
-
-  // https://stackoverflow.com/questions/32402327/
-  mainWindow.webContents.setWindowOpenHandler(({url}) => {
-    shell.openExternal(url);
-    return {action: 'deny'};
-  });
-
-  // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/app.html'); // eslint-disable-line no-path-concat
-  // $FlowFixMe[incompatible-use] found when upgrading Flow
-  mainWindow.webContents.executeJavaScript(
-    // We use this so that RN can keep relative JSX __source filenames
-    // but "click to open in editor" still works. js1 passes project roots
-    // as the argument to DevTools.
-    'window.devtools.setProjectRoots(' + JSON.stringify(projectRoots) + ')',
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
   );
+}
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
-});
+mountApp();
