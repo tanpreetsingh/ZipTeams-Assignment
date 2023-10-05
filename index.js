3256a8769fa0dac7586097e55bcb1ae2d718c180
tanpreetsingh <79357289+tanpreetsingh@users.noreply.github.com>
2023-10-05 13:33:00 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,62 +1,26 @@
-const React = window.React;
-const fixturePath = window.location.pathname;
+// This is a testing playground for our lint rules.
 
-/**
- * A simple routing component that renders the appropriate
- * fixture based on the location pathname.
- */
-class FixturesPage extends React.Component {
-  static defaultProps = {
-    fixturePath: fixturePath === '/' ? '/home' : fixturePath,
-  };
+// 1. Run yarn && yarn start
+// 2. "File > Add Folder to Workspace" this specific folder in VSCode with ESLint extension
+// 3. Changes to the rule source should get picked up without restarting ESLint server
 
-  state = {
-    isLoading: true,
-    error: null,
-    Fixture: null,
-  };
-
-  componentDidMount() {
-    this.loadFixture();
-  }
-
-  async loadFixture() {
-    const {fixturePath} = this.props;
-
-    try {
-      const module = await import(`.${fixturePath}`);
-
-      this.setState({Fixture: module.default});
-    } catch (error) {
-      console.error(error);
-      this.setState({error});
-    } finally {
-      this.setState({isLoading: false});
-    }
-  }
-
-  render() {
-    const {Fixture, error, isLoading} = this.state;
-
-    if (isLoading) {
-      return null;
-    }
-
-    if (error) {
-      return <FixtureError error={error} />;
-    }
-
-    return <Fixture />;
-  }
+function Comment({comment, commentSource}) {
+  const currentUserID = comment.viewer.id;
+  const environment = RelayEnvironment.forUser(currentUserID);
+  const commentID = nullthrows(comment.id);
+  useEffect(() => {
+    const subscription = SubscriptionCounter.subscribeOnce(
+      `StoreSubscription_${commentID}`,
+      () =>
+        StoreSubscription.subscribe(
+          environment,
+          {
+            comment_id: commentID,
+          },
+          currentUserID,
+          commentSource
+        )
+    );
+    return () => subscription.dispose();
+  }, [commentID, commentSource, currentUserID, environment]);
 }
-
-function FixtureError({error}) {
-  return (
-    <section>
-      <h2>Error loading fixture</h2>
-      <p>{error.message}</p>
-    </section>
-  );
-}
-
-export default FixturesPage;
