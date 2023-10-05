16988308c089d85fff4653a188a8faf2ae4d919b
tanpreetsingh <79357289+tanpreetsingh@users.noreply.github.com>
2023-10-05 13:38:15 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,26 +1,59 @@
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
+import * as React from 'react';
 
-import Html from './Html';
-import BigComponent from './BigComponent';
+import Container from './Container.js';
 
-export default function App({assets, title}) {
-  const components = [];
+import {Counter} from './Counter.js';
+import {Counter as Counter2} from './Counter2.js';
+import AsyncModule from './cjs/Counter3.js';
+const Counter3 = await(AsyncModule);
 
-  for (let i = 0; i <= 250; i++) {
-    components.push(<BigComponent key={i} />);
-  }
+import ShowMore from './ShowMore.js';
+import Button from './Button.js';
+import Form from './Form.js';
+import {Dynamic} from './Dynamic.js';
+import {Client} from './Client.js';
 
+import {Note} from './cjs/Note.js';
+
+import {like, greet, increment} from './actions.js';
+
+import {getServerState} from './ServerState.js';
+
+export default async function App() {
+  const res = await fetch('http://localhost:3001/todos');
+  const todos = await res.json();
   return (
-    <Html assets={assets} title={title}>
-      <h1>{title}</h1>
-      {components}
-      <h1>all done</h1>
-    </Html>
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Flight</title>
+      </head>
+      <body>
+        <Container>
+          <h1>{getServerState()}</h1>
+          <Counter incrementAction={increment} />
+          <Counter2 incrementAction={increment} />
+          <Counter3 incrementAction={increment} />
+          <ul>
+            {todos.map(todo => (
+              <li key={todo.id}>{todo.text}</li>
+            ))}
+          </ul>
+          <ShowMore>
+            <p>Lorem ipsum</p>
+          </ShowMore>
+          <Form action={greet} />
+          <div>
+            <Button action={like}>Like</Button>
+          </div>
+          <div>
+            loaded statically: <Dynamic />
+          </div>
+          <Client />
+          <Note />
+        </Container>
+      </body>
+    </html>
   );
 }
--- index.js
+++ index.js
@@ -1,12 +1,56 @@
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
+import * as React from 'react';
+import {use, Suspense, useState, startTransition} from 'react';
+import ReactDOM from 'react-dom/client';
+import {createFromFetch, encodeReply} from 'react-server-dom-webpack/client';
 
-import {hydrateRoot} from 'react-dom/client';
-import App from './App';
+// TODO: This should be a dependency of the App but we haven't implemented CSS in Node yet.
+import './style.css';
 
-hydrateRoot(document, <App assets={window.assetManifest} />);
+let updateRoot;
+async function callServer(id, args) {
+  const response = fetch('/', {
+    method: 'POST',
+    headers: {
+      Accept: 'text/x-component',
+      'rsc-action': id,
+    },
+    body: await encodeReply(args),
+  });
+  const {returnValue, root} = await createFromFetch(response, {callServer});
+  // Refresh the tree with the new RSC payload.
+  startTransition(() => {
+    updateRoot(root);
+  });
+  return returnValue;
+}
+
+function Shell({data}) {
+  const [root, setRoot] = useState(data);
+  updateRoot = setRoot;
+  return root;
+}
+
+async function hydrateApp() {
+  const {root, returnValue, formState} = await createFromFetch(
+    fetch('/', {
+      headers: {
+        Accept: 'text/x-component',
+      },
+    }),
+    {
+      callServer,
+    }
+  );
+
+  ReactDOM.hydrateRoot(document, <Shell data={root} />, {
+    // TODO: This part doesn't actually work because the server only returns
+    // form state during the request that submitted the form. Which means it
+    // the state needs to be transported as part of the HTML stream. We intend
+    // to add a feature to Fizz for this, but for now it's up to the
+    // metaframework to implement correctly.
+    formState: formState,
+  });
+}
+
+// Remove this line to simulate MPA behavior
+hydrateApp();
