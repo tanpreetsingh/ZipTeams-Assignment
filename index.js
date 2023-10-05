c86a3c66628892ae89d132a3abc0f1c568dbeb96
tanpreetsingh <79357289+tanpreetsingh@users.noreply.github.com>
2023-10-05 14:14:47 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -7,130 +7,67 @@
  * @flow
  */
 
-import Agent from './agent';
+import * as React from 'react';
+import {Fragment, useContext, useMemo} from 'react';
+import {StoreContext} from 'react-devtools-shared/src/devtools/views/context';
+import {useSubscription} from 'react-devtools-shared/src/devtools/views/hooks';
+import {NativeStyleContext} from './context';
+import LayoutViewer from './LayoutViewer';
+import StyleEditor from './StyleEditor';
+import {TreeStateContext} from '../TreeContext';
 
-import {attach} from './renderer';
-import {attach as attachLegacy} from './legacy/renderer';
-import {hasAssignedBackend} from './utils';
+type Props = {};
 
-import type {DevToolsHook, ReactRenderer, RendererInterface} from './types';
+export default function NativeStyleEditorWrapper(_: Props): React.Node {
+  const store = useContext(StoreContext);
 
-// this is the backend that is compatible with all older React versions
-function isMatchingRender(version: string): boolean {
-  return !hasAssignedBackend(version);
-}
+  const subscription = useMemo(
+    () => ({
+      getCurrentValue: () => store.supportsNativeStyleEditor,
+      subscribe: (callback: Function) => {
+        store.addListener('supportsNativeStyleEditor', callback);
+        return () => {
+          store.removeListener('supportsNativeStyleEditor', callback);
+        };
+      },
+    }),
+    [store],
+  );
+  const supportsNativeStyleEditor = useSubscription<boolean>(subscription);
 
-export type InitBackend = typeof initBackend;
-
-export function initBackend(
-  hook: DevToolsHook,
-  agent: Agent,
-  global: Object,
-): () => void {
-  if (hook == null) {
-    // DevTools didn't get injected into this page (maybe b'c of the contentType).
-    return () => {};
+  if (!supportsNativeStyleEditor) {
+    return null;
   }
 
-  const subs = [
-    hook.sub(
-      'renderer-attached',
-      ({
-        id,
-        renderer,
-        rendererInterface,
-      }: {
-        id: number,
-        renderer: ReactRenderer,
-        rendererInterface: RendererInterface,
-        ...
-      }) => {
-        agent.setRendererInterface(id, rendererInterface);
+  return <NativeStyleEditor />;
+}
 
-        // Now that the Store and the renderer interface are connected,
-        // it's time to flush the pending operation codes to the frontend.
-        rendererInterface.flushInitialOperations();
-      },
-    ),
+function NativeStyleEditor(_: Props) {
+  const {getStyleAndLayout} = useContext(NativeStyleContext);
 
-    hook.sub('unsupported-renderer-version', (id: number) => {
-      agent.onUnsupportedRenderer(id);
-    }),
+  const {inspectedElementID} = useContext(TreeStateContext);
+  if (inspectedElementID === null) {
+    return null;
+  }
 
-    hook.sub('fastRefreshScheduled', agent.onFastRefreshScheduled),
-    hook.sub('operations', agent.onHookOperations),
-    hook.sub('traceUpdates', agent.onTraceUpdates),
+  const maybeStyleAndLayout = getStyleAndLayout(inspectedElementID);
+  if (maybeStyleAndLayout === null) {
+    return null;
+  }
 
-    // TODO Add additional subscriptions required for profiling mode
-  ];
+  const {layout, style} = maybeStyleAndLayout;
 
-  const attachRenderer = (id: number, renderer: ReactRenderer) => {
-    // only attach if the renderer is compatible with the current version of the backend
-    if (!isMatchingRender(renderer.reconcilerVersion || renderer.version)) {
-      return;
-    }
-    let rendererInterface = hook.rendererInterfaces.get(id);
-
-    // Inject any not-yet-injected renderers (if we didn't reload-and-profile)
-    if (rendererInterface == null) {
-      if (typeof renderer.findFiberByHostInstance === 'function') {
-        // react-reconciler v16+
-        rendererInterface = attach(hook, id, renderer, global);
-      } else if (renderer.ComponentTree) {
-        // react-dom v15
-        rendererInterface = attachLegacy(hook, id, renderer, global);
-      } else {
-        // Older react-dom or other unsupported renderer version
-      }
-
-      if (rendererInterface != null) {
-        hook.rendererInterfaces.set(id, rendererInterface);
-      }
-    }
-
-    // Notify the DevTools frontend about new renderers.
-    // This includes any that were attached early (via __REACT_DEVTOOLS_ATTACH__).
-    if (rendererInterface != null) {
-      hook.emit('renderer-attached', {
-        id,
-        renderer,
-        rendererInterface,
-      });
-    } else {
-      hook.emit('unsupported-renderer-version', id);
-    }
-  };
-
-  // Connect renderers that have already injected themselves.
-  hook.renderers.forEach((renderer, id) => {
-    attachRenderer(id, renderer);
-  });
-
-  // Connect any new renderers that injected themselves.
-  subs.push(
-    hook.sub(
-      'renderer',
-      ({id, renderer}: {id: number, renderer: ReactRenderer, ...}) => {
-        attachRenderer(id, renderer);
-      },
-    ),
+  return (
+    <Fragment>
+      {layout !== null && (
+        <LayoutViewer id={inspectedElementID} layout={layout} />
+      )}
+      {style !== null && (
+        <StyleEditor
+          id={inspectedElementID}
+          style={style !== null ? style : {}}
+        />
+      )}
+    </Fragment>
   );
-
-  hook.emit('react-devtools', agent);
-  hook.reactDevtoolsAgent = agent;
-  const onAgentShutdown = () => {
-    subs.forEach(fn => fn());
-    hook.rendererInterfaces.forEach(rendererInterface => {
-      rendererInterface.cleanup();
-    });
-    hook.reactDevtoolsAgent = null;
-  };
-  agent.addListener('shutdown', onAgentShutdown);
-  subs.push(() => {
-    agent.removeListener('shutdown', onAgentShutdown);
-  });
-
-  return () => {
-    subs.forEach(fn => fn());
-  };
 }
